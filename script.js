77a4d5e83312ead2ba15ca215dd429e78ea2de78
JairTorezone <jairtorezone@gmail.com>
2021-03-17 11:56:33 -0300

    Definindo as direçoes e impedido de a cobrinha sumir

--- script.js
+++ script.js
@@ -21,7 +21,24 @@ function criarCobrinha(){
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.KeyCode == 37 && direction != "right") direction = "left";
+    if(event.KeyCode == 38 && direction != "down") direction = "up";
+    if(event.KeyCode == 39 && direction != "left") direction = "right";
+    if(event.KeyCode == 40 && direction != "up") direction = "down";
+
+    
+}
+
 function iniciarJogo(){
+
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    
     criarBG();
     criarCobrinha();
 
@@ -42,5 +59,6 @@ function iniciarJogo(){
 
     snake.unshift(newHead);
 }
+iniciarJogo();
 
 let jogo = setInterval(ininiciarJogo, 100); /**iniciando o jogo 100 milisegundos */
